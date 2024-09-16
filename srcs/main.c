9938d6cccd049182cfd3a3be3af2575e0e62eaf0
Mohamed Dembele <mdembele@made-f0Ar6s8.clusters.42paris.fr>
2024-09-16 15:42:27 +0200

    main

--- srcs/main.c
+++ srcs/main.c
@@ -11,7 +11,8 @@ int check_item(char **str)
 		i = 0;
 		while (str[j][i])
 		{
-			if (str[j][i] != '1' && str[j][i] != '0' && str[j][i] != 'N')
+			if (str[j][i] != '1' && str[j][i] != '0' 
+				&& str[j][i] != 'N' && str[j][i] != 'P')
 				return(EXIT_FAILURE);
 			i++;
 		}
@@ -46,9 +47,13 @@ int     main(int ac, char **av)
 {
     int fd;
     t_map_data *data;
+	void *mlx_ptr;
+	void *mlx_win;
 
     if(ac == 2)
     {
+		mlx_win = NULL;
+
         if (ft_strlen(av[1]) < 5)
 			return (ft_putstr_fd("Error\nextension not valid\n", 2), 1);
 		if (ft_strncmp(av[1] + ft_strlen(av[1]) - 4, ".cub", 4) != 0)
@@ -58,9 +63,12 @@ int     main(int ac, char **av)
             return (ft_putstr_fd("Error\npermission denied\n", 2), 1);
         data = malloc(sizeof(t_map_data));
         data->fd = fd;
-        parse_map_error(data);
+        if (parse_map_error(data) == EXIT_FAILURE)
+			return (EXIT_FAILURE);
         ft_printabln(data->map, 2);
-        
+        mlx_ptr = mlx_init();
+		mlx_win = mlx_new_window(mlx_ptr, 1920, 1980, "CUBE 3D");
+		mlx_loop(mlx_ptr);
     }
     return (0);
 }
