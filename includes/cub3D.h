bae81898a4b14722e92e64f56f348859809f8e5f
dems <you@example.com>
2024-09-16 14:33:07 +0200

    first push

---
+++ includes/cub3D.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdembele <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 21:12:40 by mdembele          #+#    #+#             */
+/*   Updated: 2024/07/08 19:30:05 by mdembele         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+# include "../libft/libft.h"
+# include "../mlx/mlx.h"
+# include "../mlx/mlx_int.h"
+# include <errno.h>
+# include <stdbool.h>
+
+# define KEYPRESS 2
+# define KEYRELEASE 3
+
+# define WALL '1'
+# define GROUND '0'
+# define PLAYER 'P'
+
+# define MALLOC_FAIL "Error\nMalloc failed"
+# define OPEN_FAIL "Error\nOpen failed"
+# define MAP_KO "Error\nThe map is not correct"
+# define WINDOWSFAIL "Error\nWindows initiation failed"
+
+typedef struct map_data
+{
+	int fd;
+	char **map;
+} t_map_data;
+#endif
---
+++ srcs/main.c
@@ -0,0 +1,45 @@
+#include "../includes/cub3D.h"
+
+int parse_map_error(t_map_data *data)
+{
+    char *str;
+    char *temp;
+
+    str = ft_strdup("");
+    temp = ft_strdup("");
+
+    while (1)
+    {
+       str = get_next_line(data->fd);
+       if (str == NULL)
+            break;
+       temp = ft_re_strjoin(temp, str);
+    }
+    data->map = ft_split(temp, '\n');
+    if (check_item == EXIT_FAILURE)
+    return(0);
+    
+}
+
+int     main(int ac, char **av)
+{
+    int fd;
+    t_map_data *data;
+
+    if(ac == 2)
+    {
+        if (ft_strlen(av[1]) < 5)
+			return (ft_putstr_fd("Error\nextension not valid\n", 2), 1);
+		if (ft_strncmp(av[1] + ft_strlen(av[1]) - 4, ".cub", 4) != 0)
+            return (ft_putstr_fd("Error\nextension not valid\n", 2), 1);
+        fd = open(av[1], O_RDONLY);
+        if (fd == -1)
+            return (ft_putstr_fd("Error\npermission denied\n", 2), 1);
+        data = malloc(sizeof(t_map_data));
+        data->fd = fd;
+        parse_map_error(data);
+        ft_printabln(data->map, 2);
+        
+    }
+    return (0);
+}
