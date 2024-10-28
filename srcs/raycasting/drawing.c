4dee5350521da234c6b8f4d6a83785757cb74f53
dems <you@example.com>
2024-10-28 10:52:32 +0100

    addd

--- srcs/raycasting/drawing.c
+++ srcs/raycasting/drawing.c
@@ -19,39 +19,6 @@
 //     }
 // }
 
-void set_pixel_map(t_player *player, t_map *map, int x)
-{
-    t_cardinal_direction dir;
-    int tex_x;
-	int tex_y;
-    int color = 0;
-    double pos = 0;
-    double step = 0;
-
-    dir = ft_get_cardinal_direction(player);
-    tex_x = (int)(player->wall_x * (TEXTURE_SIZE));
-    if (player->side == 0 && player->ray_dir_x > 0)
-        tex_x = TEXTURE_SIZE - tex_x - 1;
-    if (player->side == 1 && player->ray_dir_y < 0)
-        tex_x = TEXTURE_SIZE - tex_x - 1;
-    step = 1.0 * TEXTURE_SIZE / player->line_height;
-    pos = (player->draw_start - WIN_HEIGHT / 2 + player->line_height / 2) * step;
-    while (player->draw_start < player->draw_end)
-    {
-        tex_y = (int)pos & (TEXTURE_SIZE - 1);
-        pos += step;
-        // Ensure we're not accessing out-of-bounds memory
-        if ( tex_x >= 0 && tex_x < TEXTURE_SIZE  && tex_y >= 0 && tex_y < TEXTURE_SIZE )
-        {
-            color = (map->texture_buffer)[dir][TEXTURE_SIZE * tex_y + tex_x];
-            if (dir == NORTH || dir == SOUTH)
-                color = (color >> 1) & 0x7F7F7F;
-            if (color > 0 && player->draw_start >= 0 && player->draw_start < WIN_HEIGHT && x >= 0 && x < WIN_WIDHT)
-                player->pixel_map[player->draw_start][x] = color;
-        }
-        player->draw_start++;
-    }   
-}
 
 // void	ft_draw_pixel_map(t_player *player, t_map *m, t_all *all)
 // {
@@ -84,43 +51,154 @@ void set_pixel_map(t_player *player, t_map *map, int x)
 // 	mlx_destroy_image(player->mlx_ptr, img.img);
 // }
 
+bool	create_texture_from_img(t_img_data *img, t_map *map, t_cardinal_direction dir)
+{
+	int	*pixels;
+	int	i;
+	int	j;
+
+	pixels = ft_calloc(sizeof(int), img->width * img->height);
+	if (!pixels)
+		return (false);
+	i = -1;
+	while (++i < img->height)
+	{
+		j = -1;
+		while (++j < img->width)
+			pixels[i * img->width + j] = img->addr[i * img->width + j];
+	}
+	map->texture_buffer[dir] = pixels;
+	return (true);
+}
+
+
+void init_texture_buffer(t_map *map, t_player *player, t_windows *win)
+{
+	t_img_data	tmp;
+	int		i;
+
+	(void)player;
+	//map->texture_buffer = malloc(sizeof(int ) * 5);
+	map->path_texture[0] = ft_strdup (map->NO_texture);
+	map->path_texture[1] = ft_strdup (map->SO_texture);
+	map->path_texture[2] = ft_strdup (map->EA_texture);
+	map->path_texture[3] = ft_strdup (map->WE_texture);
+	i = -1;
+	while (++i < 4)
+	{
+		tmp.img = mlx_xpm_file_to_image(win->mlx_ptr,
+				map->path_texture[i], &tmp.width, &tmp.height);
+		if (!tmp.img)
+			exit(i);
+		printf("%d | %d\n", tmp.height, tmp.width);
+		tmp.addr = (int *)mlx_get_data_addr(tmp.img,
+				&tmp.bpp, &tmp.line_len, &tmp.endian);
+		create_texture_from_img(&tmp, map, i);
+		mlx_destroy_image(win->mlx_ptr, tmp.img);
+	}
+}
+bool	create_texture_from_img(t_img_data *img, t_map *map, t_cardinal_direction dir)
+{
+	int	*pixels;
+	int	i;
+	int	j;
+
+	pixels = ft_calloc(sizeof(int), img->width * img->height);
+	if (!pixels)
+		return (false);
+	i = -1;
+	while (++i < img->height)
+	{
+		j = -1;
+		while (++j < img->width)
+			pixels[i * img->width + j] = img->addr[i * img->width + j];
+	}
+	map->texture_buffer[dir] = pixels;
+	return (true);
+}
+
+
+void init_texture_buffer(t_map *map, t_player *player, t_windows *win)
+{
+	t_img_data	tmp;
+	int		i;
+
+	(void)player;
+	//map->texture_buffer = malloc(sizeof(int ) * 5);
+	map->path_texture[0] = ft_strdup (map->NO_texture);
+	map->path_texture[1] = ft_strdup (map->SO_texture);
+	map->path_texture[2] = ft_strdup (map->EA_texture);
+	map->path_texture[3] = ft_strdup (map->WE_texture);
+	i = -1;
+	while (++i < 4)
+	{
+		tmp.img = mlx_xpm_file_to_image(win->mlx_ptr,
+				map->path_texture[i], &tmp.width, &tmp.height);
+		if (!tmp.img)
+			exit(i);
+		printf("%d | %d\n", tmp.height, tmp.width);
+		tmp.addr = (int *)mlx_get_data_addr(tmp.img,
+				&tmp.bpp, &tmp.line_len, &tmp.endian);
+		create_texture_from_img(&tmp, map, i);
+		mlx_destroy_image(win->mlx_ptr, tmp.img);
+	}
+}
+float nor_angle(float angle) // normalize the angle
+{
+	if (angle < 0)
+		angle += (2 * M_PI);
+	if (angle > (2 * M_PI))
+		angle -= (2 * M_PI);
+	return (angle);
+}
+
+char 	*get_color(t_all *all, int flag)	// get the color of the wall
+{
+	all->ray->ray_ngl = nor_angle(all->ray->ray_ngl); // normalize the angle
+	if (flag == 0)
+	{
+		if (all->ray->ray_ngl > M_PI / 2 && all->ray->ray_ngl < 3 * (M_PI / 2))
+			return (all->dt->WE_texture); // west wall
+		else
+			return (all->dt->EA_texture); // east wall
+	}
+	else
+	{
+		if (all->ray->ray_ngl > 0 && all->ray->ray_ngl < M_PI)
+			return (all->dt->SO_texture); // south wall
+		else
+			return (all->dt->NO_texture); // north wall
+	}
+}
+
+
+void	draw_wall(t_all *all, int ray, int t_pix, int b_pix)	// draw the wall
+{
+	char *text;
+
+	color = get_color(all, all->ray->flag);
+	while (t_pix < b_pix)
+    {
+
+		my_mlx_pixel_put(all, ray, t_pix++, color);
+    }
+}
+
 void set_pixel_map(t_all *all, int x)
 {
-    t_cardinal_direction dir;
-    int tex_x;
-	int tex_y;
-    int color = 0;
-    double pos = 0;
-    double step = 0;
-    double wall_h;
     double	b_pix;
 	double	t_pix;
-
-    dir = ft_get_cardinal_direction(player);
+    double wall_h;
 
     all->ray->distance *= cos(nor_angle(all->ray->ray_ngl - all->ply->angle)); // fix the fisheye
-	wall_h = (TILE_SIZE / mlx->ray->distance) * ((S_W / 2) / tan(mlx->ply->fov_rd / 2)); // get the wall height
+	wall_h = (TILE_SIZE / all->ray->distance) * ((S_W / 2) / tan(all->ply->fov_rd / 2)); // get the wall height
     b_pix = (S_H / 2) + (wall_h / 2); // get the bottom pixel
 	t_pix = (S_H / 2) - (wall_h / 2); // get the top pixel
     if (b_pix > S_H) // check the bottom pixel
 		b_pix = S_H;
 	if (t_pix < 0) // check the top pixel
 		t_pix = 0;
-    while (t_pix < b_pix)
-    {
-        tex_y = (int)pos & (TILE_SIZE - 1);
-        pos += step;
-        // Ensure we're not accessing out-of-bounds memory
-        if ( tex_x >= 0 && tex_x < TILE_SIZE  && tex_y >= 0 && tex_y < TILE_SIZE )
-        {
-            color = (map->texture_buffer)[dir][TILE_SIZE * tex_y + tex_x];
-            if (dir == NORTH || dir == SOUTH)
-                color = (color >> 1) & 0x7F7F7F;
-            if (color > 0 && player->draw_start >= 0 && player->draw_start < WIN_HEIGHT && x >= 0 && x < WIN_WIDHT)
-                player->pixel_map[player->draw_start][x] = color;
-        }
-        player->draw_start++;
-    }   
+   draw_wall
 }
 
 void	ft_draw_pixel_map(t_player *player, t_map *m, t_all *all)
