920232e5fa3365a6a0ccf89c01b720eb1b7945af
Idrissa Baby <ibaby@made-f0Ar8s11.clusters.42paris.fr>
2024-11-07 16:25:29 +0100

    finished

--- srcs/parsing/parsing_utils1.c
+++ srcs/parsing/parsing_utils1.c
@@ -6,7 +6,7 @@
 /*   By: ibaby <ibaby@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 10:02:52 by ibaby             #+#    #+#             */
-/*   Updated: 2024/11/06 13:58:27 by ibaby            ###   ########.fr       */
+/*   Updated: 2024/11/07 16:17:33 by ibaby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	open_map(char *map_path)
 		return (double_err(map_path, ": invalid map format", false), -1);
 	fd = open(map_path, O_RDONLY);
 	if (fd == -1)
-		return (print_err("open_map: open: ", true), -1);
+		return (print_err("open_map: open", true), -1);
 	return (fd);
 }
 
